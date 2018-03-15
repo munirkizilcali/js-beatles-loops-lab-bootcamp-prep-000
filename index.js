4e62af6e5e34b39840567fd2bd67b5c8e97b45dd
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-15 20:29:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var gecici = []
   var i
-  for (i = 0; i <= musicians.length; i++) {
+  for (i = 0; i < musicians.length; i++) {
     gecici[i] = `${musicians[i]} plays ${instruments[i]}`
   }
   return gecici
